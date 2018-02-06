55d02edbba9d09f9e46cab621044f81059629f7f
gordiig <gordiig@gmail.com>
2018-02-06 20:39:25 +0300

    Rewrite NON_IDEMS_create

--- SQL/Create_scripts/NON_ITEMS_create.sql
+++ SQL/Create_scripts/NON_ITEMS_create.sql
@@ -1,38 +1,66 @@
--- Dropping before create
-DROP TABLE IF EXISTS Category_Subcategory;
-DROP TABLE IF EXISTS Subcategory_ItemTable;
-DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS Subcategory;
-DROP TABLE IF EXISTS ItemTableNames;
+-- Drop all tables
+DROP TABLE IF EXISTS Item_User;
+DROP TABLE IF EXISTS Item_Subcategory;
+DROP TABLE IF EXISTS Subcategory_Category;
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Subcategories;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Users;
 
+-- Creating
+CREATE TABLE Items
+(
+  ID SERIAL PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Date_added DATE DEFAULT current_date NOT NULL,
+  Price MONEY,
+  Phone_Number VARCHAR(63) NOT NULL,
+  Imgs_Url VARCHAR(1023)
+);
 
--- Creating all non-items tables
-CREATE TABLE Category
+CREATE TABLE Subcategories
+(
+  ID SERIAL PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Img_Url VARCHAR(1024)
+);
+
+CREATE TABLE Categories
 (
   Name VARCHAR(255) PRIMARY KEY,
-  Img_Link VARCHAR(1023)
+  Img_Url VARCHAR(1024)
 );
 
-CREATE TABLE Subcategory
+CREATE TABLE Users
 (
-  Name VARCHAR(255) PRIMARY KEY,
-  Img_Link VARCHAR(1023)
+  ID SERIAL PRIMARY KEY,
+  User_Name VARCHAR(31) DEFAULT CONCAT('USER', cast(random() AS TEXT)) NOT NULL,
+  First_Name VARCHAR(63),
+  Last_Name VARCHAR(63),
+  Date_Of_Registration DATE DEFAULT current_date NOT NULL,
+  Password VARCHAR(127) NOT NULL,
+  Img_URL VARCHAR(1023),
+  About VARCHAR(1023),
+  Phone_Number VARCHAR(63) NOT NULL,
+  EMAIL VARCHAR(127) NOT NULL,
+  City VARCHAR(63) NOT NULL
 );
 
-CREATE TABLE ItemTableNames
+  -- Connections
+CREATE TABLE Item_User
 (
-  ID INT PRIMARY KEY,
-  Table_Name VARCHAR(255)
+  Item_ID INT PRIMARY KEY REFERENCES Items(ID),
+  User_ID INT REFERENCES Users(ID)
 );
 
-CREATE TABLE Category_Subcategory
+CREATE TABLE Item_Subcategory
 (
-  Category_Name VARCHAR(255) REFERENCES category(name),
-  Subcategory_Name VARCHAR(255) REFERENCES subcategory(name)
+  Item_ID INT PRIMARY KEY REFERENCES Items(ID),
+  Subcategory_ID INT REFERENCES Subcategories(ID)
 );
 
-CREATE TABLE Subcategory_ItemTable
+CREATE TABLE Subcategory_Category
 (
-  Subcategory_Name VARCHAR(255) REFERENCES subcategory(name),
-  Item_Table_ID INT REFERENCES itemtablenames(id)
+  Subcategory_ID INT PRIMARY KEY REFERENCES Subcategories(ID),
+  Category_ID VARCHAR(255) REFERENCES Categories(Name)
 );
