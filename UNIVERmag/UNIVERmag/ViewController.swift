b8245789aa642302829c898d3cbd67ddb28d8a6a
gordiig <gordiig@gmail.com>
2018-03-05 17:33:50 +0300

    Added mainVC and performing basic username/password checking

--- UNIVERmag/UNIVERmag/ViewController.swift
+++ UNIVERmag/UNIVERmag/ViewController.swift
@@ -22,7 +22,31 @@ class ViewController: UIViewController {
     
     @IBAction func logInButClicked(_ sender: Any)
     {
-        usernameTextField.text = "Clicked"
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        if username.isEmpty || password.isEmpty
+        {
+            let alert = UIAlertController(title: "Error", message: "Enter username and password!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        if username == "admin" && password == "admin"
+        {
+            guard let mainVC = self.storyboard?.instantiateViewController(withIdentifier: "mainTapBarViewController") else
+            {
+                let alert = UIAlertController(title: "Error", message: "Something wrong with entering mainVC", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            self.present(mainVC, animated: true, completion: nil)
+        }
     }
     
     override func didReceiveMemoryWarning()
