78be0245fbe2955b6ef39fb59a313a53cb9e5123
gordiig <gordiig@gmail.com>
2018-03-05 12:53:45 +0300

    Added action on LogIn click

--- UNIVERmag/UNIVERmag/ViewController.swift
+++ UNIVERmag/UNIVERmag/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    
+    @IBAction func logInButClicked(_ sender: Any)
+    {
+        usernameTextField.text = "Clicked"
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
