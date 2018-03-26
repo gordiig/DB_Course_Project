de85857665138433ff716530398ed2360e76d11d
gordiig <gordiig@gmail.com>
2018-03-26 16:57:37 +0300

    Added currentUserSingleton

---
+++ UNIVERmag/UNIVERmag/Users.swift
@@ -0,0 +1,14 @@
+//
+//  Users.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 26.03.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import Foundation
+
+class CurrentUser
+{
+    private(set) static var getUser: User = User()
+}
