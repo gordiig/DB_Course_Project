76534231094ea5abdf56fc1e0415229b5b4e687d
gordiig <gordiig@gmail.com>
2018-04-12 08:39:39 +0300

    Changed protocol

--- UNIVERmag/UNIVERmag/Protocols/Alertable.swift
+++ UNIVERmag/UNIVERmag/Protocols/Alertable.swift
@@ -11,16 +11,5 @@ import UIKit
 
 protocol Alertable
 {
-    func showAlert(title: String, withString str: String)
-}
-
-extension Alertable
-{
-    func showAlert(controller: UIViewController, title: String = "Error", withString str: String)
-    {
-        let alert = UIAlertController(title: title, message: str, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        
-        controller.present(alert, animated: true, completion: nil)
-    }
+    func showAlert(controller: UIViewController, title: String, withString str: String)
 }
