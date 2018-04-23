d98fe0fa785fdca1bbb5e66e0d5b2b855e2cf6ed
gordiig <gordiig@gmail.com>
2018-04-23 19:45:02 +0300

    Added After_item_user_delete

--- SQL/Triggers/DELETE_ALL_TRIGGER.sql
+++ SQL/Triggers/DELETE_ALL_TRIGGER.sql
@@ -1,8 +1,10 @@
 DROP TRIGGER IF EXISTS restart_item_sequence_after_delete ON items;
 DROP TRIGGER IF EXISTS restart_subcategories_sequence_after_delete ON subcategories;
+DROP TRIGGER IF EXISTS delete_from_item_after_delete_from_item_user ON item_user;
 -- DROP TRIGGER IF EXISTS restart_users_sequence_after_delete ON users;
 DROP FUNCTION IF EXISTS restart_item_sequence();
 DROP FUNCTION IF EXISTS restart_subcategories_sequence();
+DROP FUNCTION IF EXISTS delete_from_item();
 -- DROP FUNCTION IF EXISTS restart_users_sequence();
 
 -- ITEMS
@@ -36,6 +38,20 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER restart_subcategories_sequence_after_delete
   AFTER DELETE ON subcategories
     EXECUTE PROCEDURE restart_subcategories_sequence();
+
+
+-- DELETE FROM item_user AFTER DELETE IN items
+CREATE OR REPLACE FUNCTION delete_from_item() RETURNS TRIGGER AS $$
+BEGIN
+
+  DELETE FROM items WHERE ID = old.item_ID;
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_from_item_after_delete_from_item_user
+  AFTER DELETE ON item_user FOR EACH ROW
+    EXECUTE PROCEDURE delete_from_item();
 
 -- -- USERS
 -- CREATE OR REPLACE FUNCTION restart_users_sequence() RETURNS TRIGGER AS $$
