cfdb73e496601f937060a31cdcdb8d940b0038f9
gordiig <gordiig@gmail.com>
2018-04-23 19:50:13 +0300

    Added deleting myItems

--- UNIVERmag/UNIVERmag/VC/MyItemsViewController.swift
+++ UNIVERmag/UNIVERmag/VC/MyItemsViewController.swift
@@ -71,6 +71,14 @@ class MyItemsViewController: UIViewController, UITableViewDelegate, UITableViewD
         cell?.isSelected = false
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            self.deleteWebTask(indexPath.row)
+        }
+    }
+    
     
     // MARK: - Web task
     func webTask()
@@ -135,6 +143,64 @@ class MyItemsViewController: UIViewController, UITableViewDelegate, UITableViewD
                 DispatchQueue.main.async
                 {
                     self.refreshControl.endRefreshing()
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func deleteWebTask(_ row: Int)
+    {
+        let currentUser = CurrentUser.getUser
+        let username = currentUser.username
+        let password = currentUser.password
+        
+        let idToDelete = items[row].ID
+        
+        let finalURL = URL(string: "https://sql-handler.herokuapp.com/handler/delete_shopping_item/\(username)/\(password)/\(idToDelete)")!
+        let urlRequest = URLRequest(url: finalURL)
+        let urlSession = URLSession(configuration: .default)
+        let task = urlSession.dataTask(with: urlRequest)
+        {
+            (data, response, error) in
+            
+            if error != nil
+            {
+                DispatchQueue.main.async
+                {
+                    self.showAlert(withString: "Can't get userinfo. Please try again!:\n \(error!.localizedDescription)")
+                }
+                print("Error in GET:\n \(error!.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else
+            {
+                DispatchQueue.main.async
+                {
+                    self.showAlert(withString: "Error in downloaded data! Please try again!\n")
+                }
+                print("Error in downloaded data:\n")
+                return
+            }
+            
+            let ans = String(data: data, encoding: .utf8)
+            if ans?.first == "0"
+            {
+                DispatchQueue.main.async
+                {
+                    self.items.remove(at: row)
+                    
+                    self.searchBar(self.searchBar, textDidChange: self.searchBar.text ?? "")
+                    self.tableView.reloadData()
+                }
+            }
+            else
+            {
+                DispatchQueue.main.async
+                {
+                    self.showAlert(withString: "Error while deleting item!\n")
                 }
             }
         }
