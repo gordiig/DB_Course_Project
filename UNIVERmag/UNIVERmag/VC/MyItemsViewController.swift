c29c32979562f3f082a5dac1c862aaf1688ec1fc
gordiig <gordiig@gmail.com>
2018-04-30 18:06:06 +0300

    Moved deleteWebTask()

--- UNIVERmag/UNIVERmag/VC/MyItemsViewController.swift
+++ UNIVERmag/UNIVERmag/VC/MyItemsViewController.swift
@@ -13,8 +13,8 @@ class MyItemsViewController: UserItemsViewController
 
     override func viewDidLoad()
     {
-        super.viewDidLoad()
         user = CurrentUser.getUser
+        super.viewDidLoad()
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
@@ -23,6 +23,68 @@ class MyItemsViewController: UserItemsViewController
         {
             self.deleteWebTask(indexPath.row)
         }
+    }
+    
+    func deleteWebTask(_ row: Int)
+    {
+        let currentUser = CurrentUser.getUser
+        let username = currentUser.username
+        let password = currentUser.password!
+        
+        let idToDelete = items[row].ID
+        
+        let finalURL = URL(string: "https://sql-handler.herokuapp.com/handler/delete_shopping_item/\(username)/\(password)/\(idToDelete)")!
+        let urlRequest = URLRequest(url: finalURL)
+        let urlSession = URLSession(configuration: .default)
+        let task = urlSession.dataTask(with: urlRequest)
+        {
+            (data, response, error) in
+            
+            if error != nil
+            {
+                DispatchQueue.main.async
+                    {
+                        self.showAlert(withString: "Can't get userinfo. Please try again!:\n \(error!.localizedDescription)")
+                }
+                print("Error in GET:\n \(error!.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else
+            {
+                DispatchQueue.main.async
+                    {
+                        self.showAlert(withString: "Error in downloaded data! Please try again!\n")
+                }
+                print("Error in downloaded data:\n")
+                return
+            }
+            
+            let ans = String(data: data, encoding: .utf8)
+            if ans?.first == "0"
+            {
+                DispatchQueue.main.async
+                    {
+                        self.items.remove(at: row)
+                        
+                        self.searchBar(self.searchBar, textDidChange: self.searchBar.text ?? "")
+                        
+                        // self.tableView.reloadData()
+                        let range = NSMakeRange(0, self.tableView.numberOfSections)
+                        let sections = NSIndexSet(indexesIn: range)
+                        self.tableView.reloadSections(sections as IndexSet, with: .automatic)
+                }
+            }
+            else
+            {
+                DispatchQueue.main.async
+                    {
+                        self.showAlert(withString: "Error while deleting item!\n")
+                }
+            }
+        }
+        
+        task.resume()
     }
 
 }
--- UNIVERmag/UNIVERmag/VC/UserItemsViewController.swift
+++ UNIVERmag/UNIVERmag/VC/UserItemsViewController.swift
@@ -14,9 +14,9 @@ class UserItemsViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var searchBar: UISearchBar!
     var items = [ShoppingItem]()
     var showingItems = [ShoppingItem]()
-    var user = CurrentUser.getUser
+    var user = User()
+    @IBOutlet weak var addBut: UIBarButtonItem!
     private let refreshControl = UIRefreshControl()
-    
     
     override func viewDidLoad()
     {
@@ -29,7 +29,10 @@ class UserItemsViewController: UIViewController, UITableViewDelegate, UITableVie
         
         refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
         tableView.refreshControl = refreshControl
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
         self.title = "\(user.username)'s items"
         
         let username = user.username
@@ -147,68 +150,6 @@ class UserItemsViewController: UIViewController, UITableViewDelegate, UITableVie
         task.resume()
     }
     
-    func deleteWebTask(_ row: Int)
-    {
-        let currentUser = CurrentUser.getUser
-        let username = currentUser.username
-        let password = currentUser.password!
-        
-        let idToDelete = items[row].ID
-        
-        let finalURL = URL(string: "https://sql-handler.herokuapp.com/handler/delete_shopping_item/\(username)/\(password)/\(idToDelete)")!
-        let urlRequest = URLRequest(url: finalURL)
-        let urlSession = URLSession(configuration: .default)
-        let task = urlSession.dataTask(with: urlRequest)
-        {
-            (data, response, error) in
-            
-            if error != nil
-            {
-                DispatchQueue.main.async
-                {
-                    self.showAlert(withString: "Can't get userinfo. Please try again!:\n \(error!.localizedDescription)")
-                }
-                print("Error in GET:\n \(error!.localizedDescription)")
-                return
-            }
-            
-            guard let data = data else
-            {
-                DispatchQueue.main.async
-                {
-                    self.showAlert(withString: "Error in downloaded data! Please try again!\n")
-                }
-                print("Error in downloaded data:\n")
-                return
-            }
-            
-            let ans = String(data: data, encoding: .utf8)
-            if ans?.first == "0"
-            {
-                DispatchQueue.main.async
-                {
-                    self.items.remove(at: row)
-                    
-                    self.searchBar(self.searchBar, textDidChange: self.searchBar.text ?? "")
-                    
-                    // self.tableView.reloadData()
-                    let range = NSMakeRange(0, self.tableView.numberOfSections)
-                    let sections = NSIndexSet(indexesIn: range)
-                    self.tableView.reloadSections(sections as IndexSet, with: .automatic)
-                }
-            }
-            else
-            {
-                DispatchQueue.main.async
-                {
-                    self.showAlert(withString: "Error while deleting item!\n")
-                }
-            }
-        }
-        
-        task.resume()
-    }
-    
     
     // MARK: - UISearchBar
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
@@ -241,8 +182,7 @@ class UserItemsViewController: UIViewController, UITableViewDelegate, UITableVie
     {
         self.items = [ShoppingItem]()
         
-        let currentUser = CurrentUser.getUser
-        let username = currentUser.username
+        let username = user.username
         self.webTask(username: username)
     }
     
