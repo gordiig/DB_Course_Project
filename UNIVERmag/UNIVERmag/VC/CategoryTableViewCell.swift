41fcac419f525b3b8ab263789d063d687cc33ea6
gordiig <gordiig@gmail.com>
2018-05-05 22:02:35 +0300

    Added classes for table view cells

---
+++ UNIVERmag/UNIVERmag/VC/CategoryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryTableViewCell.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 05.05.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
