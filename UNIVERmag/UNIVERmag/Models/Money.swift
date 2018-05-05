fa167943fb599c7ea5bff65eee448e64e1813289
gordiig <gordiig@gmail.com>
2018-05-05 19:39:16 +0300

    Added some stuff to Money

--- UNIVERmag/UNIVERmag/Models/Money.swift
+++ UNIVERmag/UNIVERmag/Models/Money.swift
@@ -10,21 +10,60 @@ import Foundation
 
 class Money: NSObject
 {
-    var dollar: Int = 0
+    var dollars: Int = 0
     var cents: Int = 0
+    private(set) var powForCents = 0
+    var currencySign = "₽"
     
-    init(_ dollar: Int, cents: Int = 0)
+    override public var description: String
     {
-        super.init()
+        var ans = "\(dollars)."
+        for _ in 0 ..< powForCents-1
+        {
+            ans += "0"
+        }
+        ans += "\(cents)\(currencySign)"
         
-        self.dollar = dollar
-        self.cents = cents
+        return ans
     }
     
-    init(_ money: Float)
+    init(_ dollar: Int, cents: Int = 0, powerForCents: Int = 1)
     {
         super.init()
         
+        self.dollars = dollar
+        self.cents = cents
         
+        if powerForCents >= 1
+        {
+            self.powForCents = powerForCents
+        }
+    }
+    
+    init(_ money: Double)
+    {
+        super.init()
+        
+        let sep = money.integersSeparatedByPoint()
+        dollars = sep.dollars
+        cents = sep.cents
+        powForCents = sep.powerForCents
+    }
+    
+    func toDecimal() -> Decimal
+    {
+        let newFloat = Decimal(cents) / pow(10, powForCents)
+        
+        var ans = Decimal(abs(dollars)) + newFloat
+        ans *= Decimal(abs(dollars) / dollars)
+        
+        return ans
+    }
+    
+    func toDouble() -> Double
+    {
+        let decimal = self.toDecimal()
+        let ans = (decimal as NSDecimalNumber).doubleValue
+        return ans
     }
 }
