976cb7c1b4a6a4f09502c0d0a6bf7b1ffb036e6b
Gordiig <gordiig@gmail.com>
2018-05-07 10:27:30 +0300

    Added menuTableView class

---
+++ UNIVERmag/UNIVERmag/TableViews/MenuTableView.swift
@@ -0,0 +1,30 @@
+//
+//  MenuTableView.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 07.05.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableView: UITableView, UITableViewDelegate, UITableViewDataSource
+{
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        <#code#>
+    }
+}
