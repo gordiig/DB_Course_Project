228251c2024aa75b0a1685260ec4fa37a8f9d77b
Gordiig <gordiig@gmail.com>
2018-05-07 10:52:22 +0300

    implemented menuTableView

--- UNIVERmag/UNIVERmag/TableViews/MenuTableView.swift
+++ UNIVERmag/UNIVERmag/TableViews/MenuTableView.swift
@@ -10,21 +10,80 @@ import UIKit
 
 class MenuTableView: UITableView, UITableViewDelegate, UITableViewDataSource
 {
-    
-    
+    var categories = CurrentCategories.cur
+    {
+        didSet
+        {
+            selectedSubcats = [Bool]()
+            for _ in 0 ..< categories.catAndSubcatCount
+            {
+                selectedSubcats.append(false)
+            }
+            
+            self.reloadData()
+        }
+    }
+    var selectedSubcats = [Bool]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        <#code#>
+        return categories.catAndSubcatCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        <#code#>
+        if categories.categoriesIndexesInOneLayer.contains(indexPath.row)
+        {
+            guard let cell = self.dequeueReusableCell(withIdentifier: "categoriesTableView") as? CategoryTableViewCell else
+            {
+                return UITableViewCell()
+            }
+            cell.nameLabel.text = categories.inOneLayer[indexPath.row].name
+            
+            return cell
+        }
+        else
+        {
+            guard let cell = self.dequeueReusableCell(withIdentifier: "subcategoriesTableView") as? SubcategoryTableViewCell else
+            {
+                return UITableViewCell()
+            }
+            cell.nameLabel.text = categories.inOneLayer[indexPath.row].name
+            
+            return cell
+        }
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        <#code#>
+        let oneLayer = categories.inOneLayer
+        var cell = self.cellForRow(at: indexPath)
+        
+        let accessoryType: UITableViewCellAccessoryType = cell?.accessoryType == .none ? .checkmark : .none
+        let boolVal = accessoryType == .none ? true : false
+        
+        cell?.accessoryType = accessoryType
+        selectedSubcats[indexPath.row] = boolVal
+        
+        if oneLayer[indexPath.row].ID == nil
+        {
+            var index = indexPath.row + 1
+            let maxIndex = oneLayer.count
+            
+            while index < maxIndex && oneLayer[index].ID == nil
+            {
+                cell = self.cellForRow(at: IndexPath(row: index, section: 0))
+                cell?.accessoryType = accessoryType
+                selectedSubcats[index] = boolVal
+                index += 1
+            }
+        }
+        
     }
 }
+
+
+
+
+
+
