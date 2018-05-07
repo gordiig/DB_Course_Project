1cdec02093662d98fbc21293c8b75d28749aa7f9
Gordiig <gordiig@gmail.com>
2018-05-07 11:58:59 +0300

    Deleted Extensions

--- UNIVERmag/UNIVERmag/Protocols/Alertable.swift
+++ UNIVERmag/UNIVERmag/Protocols/Alertable.swift
@@ -11,13 +11,5 @@ import UIKit
 
 protocol Alertable
 {
-    func showAlert(controller: UIViewController, title: String, withString str: String)
-}
-
-extension Alertable
-{
-    func showAlert(controller: UIViewController, title: String = "Error", withString str: String)
-    {
-        showAlert(controller: controller, title: title, withString: str)
-    }
+    func showAlert(title: String, withString str: String)
 }
