92d015a19d3e3c4be3ac197aeaa2a2205d6de725
Gordiig <gordiig@gmail.com>
2018-05-07 14:55:52 +0300

    Added Singletons folder

---
+++ UNIVERmag/UNIVERmag/Singletons/CurrentWebTasker.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentWebTasker.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 07.05.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import Foundation
+
+class CurrentWebTasker
+{
+    private(set) static var tasker = WebTasker()
+}
