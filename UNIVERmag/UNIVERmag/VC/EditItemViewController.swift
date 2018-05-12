cc2e08b9c8b4636a3d47e433348882e6d8c879c7
Gordiig <gordiig@gmail.com>
2018-05-12 11:22:26 +0300

    Implemented EditVC (without webTask)

--- UNIVERmag/UNIVERmag/VC/EditItemViewController.swift
+++ UNIVERmag/UNIVERmag/VC/EditItemViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EditItemViewController: UIViewController, Alertable
+class EditItemViewController: UIViewController, Alertable, UITextFieldDelegate
 {
     var item = ShoppingItem()
     
@@ -20,19 +20,59 @@ class EditItemViewController: UIViewController, Alertable
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
         
+        nameTextField.delegate = self
+        priceTextField.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        nameTextField.text = item.name
+        priceTextField.text = String(describing: item.price.toDouble())
+        aboutTextField.text = item.about
+    }
+    
+    
+    // Text Field delegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+    {
+        if textField != priceTextField { return true }
+        
+        var allowedStr = "0123456789."
+        let text = textField.text ?? ""
+        if text.contains(".")
+        {
+            allowedStr.removeLast()
+        }
+        
+        let allowedCharacters = CharacterSet(charactersIn: allowedStr)
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.view.endEditing(true)
+    }
+
     
     @IBAction func submitButPressed(_ sender: Any)
     {
-        
+        // TODO: - WebTask()
     }
     
     // MARK: - Alertable
     func showAlert(title: String, withString str: String)
     {
-        <#code#>
+        let alert = UIAlertController(title: title, message: str, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
     }
-
 }
