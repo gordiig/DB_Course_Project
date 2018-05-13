77eadf4b26ecbfea05830e914abc7e9d387b7fff
Gordiig <gordiig@gmail.com>
2018-05-13 16:54:34 +0300

    Added web task

--- UNIVERmag/UNIVERmag/VC/SignUpViewController.swift
+++ UNIVERmag/UNIVERmag/VC/SignUpViewController.swift
@@ -28,7 +28,118 @@ class SignUpViewController: UIViewController, Alertable
     
     @IBAction func submitButPressed(_ sender: Any)
     {
+        guard let username = usernameTextField.text else
+        {
+            showAlert(withString: "You must enter username!")
+            return
+        }
+        if username == "" {showAlert(withString: "You must enter username!"); return}
         
+        guard let password = passwordTextField.text else
+        {
+            showAlert(withString: "You must enter password!")
+            return
+        }
+        if password == "" {showAlert(withString: "You must enter password!"); return}
+        
+        guard let city = cityTextField.text else
+        {
+            showAlert(withString: "You must enter your city!")
+            return
+        }
+        if city == "" {showAlert(withString: "You must enter your city!"); return}
+        
+        guard let email = emailTextField.text else
+        {
+            showAlert(withString: "You must enter your email!")
+            return
+        }
+        if email == "" {showAlert(withString: "You must enter your email!"); return}
+        
+        guard let phoneNumber = phoneTextField.text else
+        {
+            showAlert(withString: "You must enter your phone number!")
+            return
+        }
+        if phoneNumber == "" {showAlert(withString: "You must enter your phone number!"); return}
+        
+        let firstName = firstNameTextField.text ?? "NULL"
+        let lastName = lastNameTextField.text ?? "NULL"
+        
+        let errorHandler: (Error?) -> Void =
+        { (error) in
+            DispatchQueue.main.async
+            {
+                self.showAlert(withString: "Error occured while web task!:\n \(error!.localizedDescription)")
+            }
+        }
+        
+        let dataErrorHandler: () -> Void =
+        {
+            DispatchQueue.main.async
+            {
+                self.showAlert(withString: "Error in downloaded data! Please try again!\n")
+            }
+        }
+        
+        let succsessHandler: (Data, String?) -> Void =
+        { (data, ans) in
+            if ans?.first == "["
+            {
+                DispatchQueue.main.async
+                {
+                    if !CurrentUser.getUser.decodeFromJSON(data)
+                    {
+                        self.showAlert(withString: "Can't set current user!\n")
+                        return
+                    }
+                    
+                    self.saveUserPassword(username: username, password: password)
+                    self.goToMainVC()
+                }
+            }
+            else if ans?.first == "0"
+            {
+                DispatchQueue.main.async
+                {
+                    self.showAlert(withString: "Entered username is olready in use. Think of a different one.")
+                }
+            }
+            else
+            {
+                DispatchQueue.main.async
+                {
+                    self.showAlert(withString: "Unknown error has occured while uploading userinfo. Please try again!")
+                }
+            }
+            
+        }
+        
+        let tasker = CurrentWebTasker.tasker
+        tasker.signUpWebTask(username: username, firstName: firstName, lastName: lastName, password: password, phoneNumber: phoneNumber, email: email, city: city, errorHandler: errorHandler, dataErrorHandler: dataErrorHandler, succsessHandler: succsessHandler, deferBody: {})
+    }
+    
+    
+    // MARK: - Some privates
+    private func goToMainVC()
+    {
+        guard let mainVC = self.storyboard?.instantiateViewController(withIdentifier: "mainTapBarViewController") else
+        {
+            self.showAlert(title: "Error", withString: "Something wrong with entering mainVC")
+            return
+        }
+        
+        self.present(mainVC, animated: true, completion: nil)
+    }
+    
+    
+    // TODO: Make safe login save
+    private func saveUserPassword(username: String, password: String)
+    {
+        let defaults = UserDefaults.standard
+        
+        defaults.set(username, forKey: "Username")
+        defaults.set(password, forKey: "Password")
     }
     
     
