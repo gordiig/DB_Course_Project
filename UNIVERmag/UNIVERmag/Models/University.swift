29b54c4c5ba2c10a1e6345223523f4bd3a579380
Gordiig <gordiig@gmail.com>
2018-05-20 21:52:20 +0300

    Added University and CurrentUniversity classes

---
+++ UNIVERmag/UNIVERmag/Models/University.swift
@@ -0,0 +1,90 @@
+//
+//  University.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 20.05.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import Foundation
+
+class University: NSObject, JSONable
+{
+    var ID: Int = 1
+    var fullName: String = "Default"
+    var shortName: String = "DFLT"
+    
+    // MARK: - Codable struct
+    private struct UniversityStruct: Codable
+    {
+        var id: Int
+        var full_name: String
+        var short_name: String
+    }
+    
+    override init()
+    {
+        super.init()
+    }
+    
+    
+    // MARK: - JSONable
+    required init?(fromData data: Data)
+    {
+        super.init()
+        
+        if !decodeFromJSON(data)
+        {
+            return nil
+        }
+    }
+    
+    func decodeFromJSON(_ data: Data) -> Bool
+    {
+        let decoder = JSONDecoder()
+        
+        guard let newUniversity = try? decoder.decode(UniversityStruct.self, from: data) else
+        {
+            return false
+        }
+        self.fillFromJSONStruct(newUniversity)
+        
+        return true
+    }
+    
+    func encodeToJSON() -> Data?
+    {
+        return nil
+    }
+    
+    
+    // MARK: - Factory
+    static func universitiesFactory(fromData data: Data) -> [University]?
+    {
+        let decoder = JSONDecoder()
+        
+        guard let newUniversities = try? decoder.decode([UniversityStruct].self, from: data) else
+        {
+            return nil
+        }
+        
+        var ans = [University]()
+        for un in newUniversities
+        {
+            let newItem = University()
+            newItem.fillFromJSONStruct(un)
+            ans.append(newItem)
+        }
+        
+        return ans
+    }
+    
+    
+    // MARK: - Some privates
+    private func fillFromJSONStruct(_ val: UniversityStruct)
+    {
+        self.ID = val.id
+        self.fullName = val.full_name
+        self.shortName = val.short_name
+    }
+}
---
+++ UNIVERmag/UNIVERmag/Singletons/CurrentUniversities.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentUniversities.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 20.05.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import Foundation
+
+class CurrentUniversities
+{
+    private(set) static var cur = [University]()
+}
