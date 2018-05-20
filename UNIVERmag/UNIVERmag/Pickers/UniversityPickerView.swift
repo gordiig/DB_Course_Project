bd80a0c090a5292bd3a3fb75dec89f94a67fa5e1
Gordiig <gordiig@gmail.com>
2018-05-20 22:15:35 +0300

    Added UniversityPickerView

---
+++ UNIVERmag/UNIVERmag/Pickers/UniversityPickerView.swift
@@ -0,0 +1,29 @@
+//
+//  UniversityPickerView.swift
+//  UNIVERmag
+//
+//  Created by Dmitry Gorin on 20.05.2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import UIKit
+
+class UniversityPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource
+{
+    let curUn = CurrentUniversities.cur
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return curUn.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return curUn[row].fullName
+    }
+}
